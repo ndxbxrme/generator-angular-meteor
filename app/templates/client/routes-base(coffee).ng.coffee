da94d7a470d2f969fe2f1fcf564274aec2fa91e6
ndxbxrme <lewis_the_cat@hotmail.com>
2015-05-19 21:01:04 +0100

    better auth

--- app/templates/client/routes-base(coffee).ng.coffee
+++ app/templates/client/routes-base(coffee).ng.coffee
@@ -6,3 +6,8 @@ angular.module '<%= appname %>'
   $locationProvider.html5Mode true
   $urlRouterProvider.otherwise '/'
 ]
+<% if(auth) { %>.run ['$rootScope', '$state', ($rootScope, $state) ->
+  $rootScope.$on '$stateChangeError', (event, toState, toParams, fromState, fromParams, error) ->
+    if error is 'AUTH_REQUIRED'
+      $state.go '/'
+]<% } %>
--- app/templates/client/routes-base(js).ng.js
+++ app/templates/client/routes-base(js).ng.js
@@ -5,4 +5,10 @@ angular.module('<%= appname %>')
 .config(function($urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
-});
+})<% if(auth) { %>.run(['$rootScope', '$state', function($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('/');
+    }
+  });
+}])<% } %>;
